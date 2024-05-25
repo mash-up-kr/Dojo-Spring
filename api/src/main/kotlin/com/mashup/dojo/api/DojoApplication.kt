1cbb805caf00508f688044a4832d44fc237adf38
Heidi <heidi@daangn.com>
2024-05-26 01:20:53 +0900

    스켈레톤(테스트)코드 추가

--- api/src/main/kotlin/com/mashup/dojo/api/DojoApplication.kt
+++ api/src/main/kotlin/com/mashup/dojo/api/DojoApplication.kt
@@ -1,9 +1,11 @@
-package com.mashup.dojo
+package com.mashup.dojo.api
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.ComponentScan
 
 @SpringBootApplication
+@ComponentScan("com.mashup.dojo")
 class DojoApplication
 
 fun main(args: Array<String>) {
---
+++ service/src/main/kotlin/com/mashup/dojo/domain/DojoDomain.kt
@@ -0,0 +1,10 @@
+package com.mashup.dojo.domain
+
+class DojoDomain(
+    private val name: String
+) {
+
+    fun name(): String {
+        return name
+    }
+}
