8f0ed426ba0ac1de31f1e95c1a34790e6cbe4d6c
toychip <dlawnsgud427@naver.com>
2024-06-14 00:07:27 +0900

    fix: Restore ID field in BaseEntity & rename deleted to isDeleted

--- entity/src/main/kotlin/com/mashup/dojo/SampleEntity.kt
+++ entity/src/main/kotlin/com/mashup/dojo/SampleEntity.kt
@@ -1,5 +1,6 @@
 package com.mashup.dojo
 
+import com.mashup.dojo.base.BaseEntity
 import jakarta.persistence.Entity
 
 @Entity
--- entity/src/main/kotlin/com/mashup/dojo/base/BaseEntity.kt
+++ entity/src/main/kotlin/com/mashup/dojo/base/BaseEntity.kt
@@ -2,6 +2,9 @@ package com.mashup.dojo.base
 
 import jakarta.persistence.Column
 import jakarta.persistence.EntityListeners
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
 import jakarta.persistence.MappedSuperclass
 import org.springframework.data.annotation.CreatedBy
 import org.springframework.data.annotation.LastModifiedBy
@@ -11,6 +14,11 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener
 @EntityListeners(AuditingEntityListener::class)
 abstract class BaseEntity : BaseTimeEntity() {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    val id: Long = 0L
+    
     @CreatedBy
     @Column(updatable = false)
     var createdBy: String? = null
@@ -21,16 +29,16 @@ abstract class BaseEntity : BaseTimeEntity() {
         private set
 
     @Column(nullable = false)
-    var deleted: Boolean = false
+    var isDeleted: Boolean = false
         private set
 
     // 재활성화 - soft delete
     fun activate() {
-        this.deleted = false
+        this.isDeleted = false
     }
 
     // 비활성화 - soft delete
     fun deactivate() {
-        this.deleted = true
+        this.isDeleted = true
     }
 }
