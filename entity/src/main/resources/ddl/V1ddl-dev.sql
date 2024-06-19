5beaf90c1d79350fa16e8b8d958f88f34a407bc1
toychip <dlawnsgud427@naver.com>
2024-06-20 03:46:10 +0900

    feat: V1 dev, prod ddl (#8)

---
+++ entity/src/main/resources/ddl/V1ddl-dev.sql
@@ -0,0 +1,58 @@
+CREATE TABLE dojo_dev.question
+(
+    created_at DATETIME(6) NOT NULL,
+    id         BIGINT       NOT NULL AUTO_INCREMENT,
+    updated_at DATETIME(6) NOT NULL,
+    content    VARCHAR(255) NOT NULL,
+    target     ENUM('EITHER','FRIEND','STRANGER') NOT NULL,
+    PRIMARY KEY (id)
+) ENGINE=InnoDB;
+
+CREATE TABLE dojo_dev.profile
+(
+    is_deleted       BIT          NOT NULL,
+    created_at       DATETIME(6) NOT NULL,
+    id               BIGINT       NOT NULL AUTO_INCREMENT,
+    member_id        BIGINT,
+    updated_at       DATETIME(6) NOT NULL,
+    created_by       VARCHAR(255),
+    image_url        VARCHAR(255) NOT NULL,
+    last_modified_by VARCHAR(255),
+    PRIMARY KEY (id)
+) ENGINE=InnoDB;
+
+CREATE TABLE dojo_dev.pick
+(
+    created_at     DATETIME(6) NOT NULL,
+    from_member_id BIGINT,
+    id             BIGINT NOT NULL AUTO_INCREMENT,
+    question_id    BIGINT,
+    to_member_id   BIGINT,
+    updated_at     DATETIME(6) NOT NULL,
+    PRIMARY KEY (id)
+) ENGINE=InnoDB;
+
+CREATE TABLE dojo_dev.notification
+(
+    send_status BIT          NOT NULL,
+    created_at  DATETIME(6) NOT NULL,
+    id          BIGINT       NOT NULL AUTO_INCREMENT,
+    updated_at  DATETIME(6) NOT NULL,
+    content     VARCHAR(255) NOT NULL,
+    title       VARCHAR(255) NOT NULL,
+    topic       VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
+) ENGINE=InnoDB;
+
+CREATE TABLE dojo_dev.member
+(
+    generation INT          NOT NULL,
+    point      INT          NOT NULL,
+    created_at DATETIME(6) NOT NULL,
+    id         BIGINT       NOT NULL AUTO_INCREMENT,
+    updated_at DATETIME(6) NOT NULL,
+    name       VARCHAR(255) NOT NULL,
+    gender     ENUM('FEMALE','MALE') NOT NULL,
+    part       ENUM('ANDROID','IOS','PRODUCT_DESIGN','SPRING','WEB') NOT NULL,
+    PRIMARY KEY (id)
+) ENGINE=InnoDB;
