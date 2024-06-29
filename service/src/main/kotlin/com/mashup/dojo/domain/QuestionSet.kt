5b15702b9690eb05d352a0cd156aeefd7c3fe1d5
xonmin <han51361@gmail.com>
2024-06-30 00:30:25 +0900

    add : QuestionOrder in QuestionSet

--- service/src/main/kotlin/com/mashup/dojo/domain/QuestionSet.kt
+++ service/src/main/kotlin/com/mashup/dojo/domain/QuestionSet.kt
@@ -5,9 +5,14 @@ import java.time.LocalDateTime
 @JvmInline
 value class QuestionSetId(val value: Long)
 
+data class QuestionOrder(
+    val questionId: QuestionId,
+    val order: Int,
+)
+
 data class QuestionSet(
     val id: QuestionSetId,
-    val questionIds: List<QuestionId>,
+    val questionIds: List<QuestionOrder>,
     // 질문 발행일
     val publishedAt: LocalDateTime,
 )
