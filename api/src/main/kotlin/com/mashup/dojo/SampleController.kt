014016fcc54ac9d2da750590d42a6f6ba7b174fa
Heidi <heidi@daangn.com>
2024-06-29 14:07:16 +0900

    lint 수정

--- api/src/main/kotlin/com/mashup/dojo/SampleController.kt
+++ api/src/main/kotlin/com/mashup/dojo/SampleController.kt
@@ -25,10 +25,12 @@ class SampleController(
         summary = "Sample API",
         description = "Sample API 입니다요",
         responses = [
-            ApiResponse(responseCode = "200", description = "샘플 API 성공하면 어쩌구 데이터가 내려갑니다."),
-        ],
+            ApiResponse(responseCode = "200", description = "샘플 API 성공하면 어쩌구 데이터가 내려갑니다.")
+        ]
     )
-    fun test(@PathVariable id: Long): DojoApiResponse<Long> {
+    fun test(
+        @PathVariable id: Long,
+    ): DojoApiResponse<Long> {
         return DojoApiResponse.success(sampleUseCase.getSampleId())
     }
 }
