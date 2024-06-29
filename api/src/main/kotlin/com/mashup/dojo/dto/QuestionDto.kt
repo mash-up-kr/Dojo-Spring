195cda719e99419da0830b1f723f2e616589b54e
xonmin <han51361@gmail.com>
2024-06-29 14:37:15 +0900

    refactor : ktlint

--- api/src/main/kotlin/com/mashup/dojo/dto/QuestionDto.kt
+++ api/src/main/kotlin/com/mashup/dojo/dto/QuestionDto.kt
@@ -2,13 +2,11 @@ package com.mashup.dojo.dto
 
 import com.mashup.dojo.domain.QuestionType
 
-
 data class QuestionCreateRequest(
     val content: String,
     val type: QuestionType,
     val imageUrl: String,
 )
-
 
 data class QuestionBulkCreateRequest(
     val questionList: List<QuestionCreateRequest>,
--- service/src/main/kotlin/com/mashup/dojo/service/QuestionService.kt
+++ service/src/main/kotlin/com/mashup/dojo/service/QuestionService.kt
@@ -4,7 +4,6 @@ import com.mashup.dojo.domain.Question
 import com.mashup.dojo.domain.QuestionId
 import com.mashup.dojo.domain.QuestionType
 import io.github.oshai.kotlinlogging.KotlinLogging
-
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 import java.time.LocalDateTime
@@ -14,10 +13,13 @@ private val log = KotlinLogging.logger {}
 @Service
 @Transactional(readOnly = true)
 class QuestionService {
-
     @Transactional
-    fun createQuestion(content: String, type: QuestionType, imageUrl: String): Question {
-        // todo create questionEntity 
+    fun createQuestion(
+        content: String,
+        type: QuestionType,
+        imageUrl: String,
+    ): Question {
+        // todo create questionEntity
         // return QuestionRepository.save(content, type, imageUrl).toQuestion
         val question = sampleQuestion
 
@@ -27,13 +29,14 @@ class QuestionService {
     }
 
     companion object {
-        val sampleQuestion = Question(
-            id = QuestionId(8181818),
-            content = "세상에서 제일 멋쟁이인 사람",
-            type = QuestionType.FRIEND,
-            imageUrl = "default:url",
-            createdAt = LocalDateTime.now(),
-            deletedAt = null,
-        )
+        val sampleQuestion =
+            Question(
+                id = QuestionId(8181818),
+                content = "세상에서 제일 멋쟁이인 사람",
+                type = QuestionType.FRIEND,
+                imageUrl = "default:url",
+                createdAt = LocalDateTime.now(),
+                deletedAt = null
+            )
     }
 }
