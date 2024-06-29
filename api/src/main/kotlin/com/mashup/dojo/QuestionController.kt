3f593d81352f62675d5c0d65243864da49b59058
xonmin <han51361@gmail.com>
2024-06-29 14:53:05 +0900

    add : swagger annotation

--- api/src/main/kotlin/com/mashup/dojo/QuestionController.kt
+++ api/src/main/kotlin/com/mashup/dojo/QuestionController.kt
@@ -3,20 +3,25 @@ package com.mashup.dojo
 import com.mashup.dojo.common.DojoApiResponse
 import com.mashup.dojo.dto.QuestionCreateRequest
 import com.mashup.dojo.usecase.QuestionUseCase
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.tags.Tag
+import jakarta.validation.Valid
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
+@Tag(name = "Question", description = "질문지 관련 API 입니다")
 @RequestMapping("/question")
 @RestController
 class QuestionController(
     private val questionUseCase: QuestionUseCase,
 ) {
-    // todo dto validation by @Valid
+    // todo : add auth param
+    @Operation(summary = "create Question API", description = "질문지 생성")
     @PostMapping
     fun createQuestion(
-        @RequestBody request: QuestionCreateRequest,
+        @Valid @RequestBody request: QuestionCreateRequest,
     ): DojoApiResponse<Unit> {
         questionUseCase.create(
             QuestionUseCase.CreateCommand(
--- api/src/main/kotlin/com/mashup/dojo/dto/QuestionDto.kt
+++ api/src/main/kotlin/com/mashup/dojo/dto/QuestionDto.kt
@@ -1,13 +1,23 @@
 package com.mashup.dojo.dto
 
 import com.mashup.dojo.domain.QuestionType
+import io.swagger.v3.oas.annotations.media.Schema
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.NotNull
 
+@Schema(description = "질문 등록 요청")
 data class QuestionCreateRequest(
+    @field:NotBlank
     val content: String,
+    @field:NotNull
     val type: QuestionType,
+    @field:NotBlank
+    @Schema(description = "질문 이미지")
     val imageUrl: String,
 )
 
+@Schema(description = "질문 등록 bulk 요청")
 data class QuestionBulkCreateRequest(
+    @Schema(description = "질문 요청 list")
     val questionList: List<QuestionCreateRequest>,
 )
