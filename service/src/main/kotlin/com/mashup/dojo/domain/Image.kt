f537218db4820bf47cfce5aac9464b01bee5150d
xonmin <han51361@gmail.com>
2024-06-29 21:09:43 +0900

    revert : user image , emoji image 분리

--- service/src/main/kotlin/com/mashup/dojo/domain/Image.kt
+++ service/src/main/kotlin/com/mashup/dojo/domain/Image.kt
@@ -3,30 +3,15 @@ package com.mashup.dojo.domain
 @JvmInline
 value class ImageId(val value: Long)
 
-sealed class Image
-
-data class UserImage(
+data class Image(
     val id: ImageId,
     val url: String,
-) : Image() {
+) {
     companion object {
         val MOCK_USER_IMAGE =
-            UserImage(
+            Image(
                 id = ImageId(1),
                 url = "https://example.com/image/1"
-            )
-    }
-}
-
-data class EmojiImage(
-    val id: ImageId,
-    val url: String,
-) : Image() {
-    companion object {
-        val MOCK_EMOJI_IMAGE =
-            EmojiImage(
-                id = ImageId(2),
-                url = "https://example.com/image/2"
             )
     }
 }
