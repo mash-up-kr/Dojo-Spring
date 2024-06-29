975c6929e70b4c532e67e4ca9369cd866d065526
xonmin <han51361@gmail.com>
2024-06-29 22:19:58 +0900

    add : scheduler config

---
+++ api/src/main/kotlin/com/mashup/dojo/config/SchedulerConfig.kt
@@ -0,0 +1,25 @@
+package com.mashup.dojo.config
+
+import com.mashup.dojo.scheduler.SchedulerBasePackage
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.context.annotation.Configuration
+import org.springframework.scheduling.annotation.EnableScheduling
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor
+import java.util.concurrent.Executor
+
+@Configuration
+@EnableScheduling
+@ComponentScan(basePackageClasses = [SchedulerBasePackage::class])
+class SchedulerConfig {
+    @Bean(name = ["questionSetSchedulerExecutor"])
+    fun questionSetSchedulerExecutor(): Executor {
+        return ThreadPoolTaskExecutor().apply {
+            corePoolSize = 5
+            maxPoolSize = 5
+            queueCapacity = 20
+            setThreadNamePrefix("questionSetScheduler-")
+            setWaitForTasksToCompleteOnShutdown(true)
+        }
+    }
+}
---
+++ api/src/main/kotlin/com/mashup/dojo/scheduler/Scheduler.kt
@@ -0,0 +1,27 @@
+package com.mashup.dojo.scheduler
+
+import com.mashup.dojo.usecase.QuestionUseCase
+import io.github.oshai.kotlinlogging.KotlinLogging
+import org.springframework.scheduling.annotation.Async
+import org.springframework.scheduling.annotation.Scheduled
+import org.springframework.stereotype.Component
+import java.time.LocalDateTime
+
+private val log = KotlinLogging.logger {}
+
+@Component
+class Scheduler(
+    private val questionUseCase: QuestionUseCase,
+) {
+    @Scheduled(cron = SCHEDULED_CRON)
+    @Async("questionSetSchedulerExecutor")
+    fun createQuestionSet() {
+        log.info { "=== Start Create questionSet at ${LocalDateTime.now()}. ===" }
+        questionUseCase.createQuestionSet()
+        log.info { "=== Done Create questionSet at ${LocalDateTime.now()}. ===" }
+    }
+    
+    companion object {
+        private const val SCHEDULED_CRON = "0 0 9,21 * * *"
+    }
+}
