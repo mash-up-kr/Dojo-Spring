b97fbc1cd5c80aa945227712d8d57016f2598b79
toychip <dlawnsgud427@naver.com>
2024-06-29 17:39:09 +0900

    feat: api SheetController

---
+++ api/src/main/kotlin/com/mashup/dojo/sheet/dto/response/SheetResponse.kt
@@ -0,0 +1,14 @@
+package com.mashup.dojo.sheet.dto.response
+
+data class SheetResponse(
+    val questionSetId: Long,
+    val questionId: Long,
+    val imageUrl: String,
+    val candidates: List<MemberResponse>
+)
+
+data class MemberResponse(
+    val memberId: Long,
+    val memberName: String,
+    val order: Int
+)
