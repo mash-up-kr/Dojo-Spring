b262f8651ce4e419d82044cf7bb0c8c7e4c6276f
toychip <dlawnsgud427@naver.com>
2024-06-29 20:25:38 +0900

    refactor: extract static method for mock Question creation

--- service/src/main/kotlin/com/mashup/dojo/service/QuestionService.kt
+++ service/src/main/kotlin/com/mashup/dojo/service/QuestionService.kt
@@ -7,19 +7,26 @@ import org.springframework.stereotype.Service
 import java.time.LocalDateTime
 
 interface QuestionService {
-    fun getQuestion(questionLongId: QuestionId): Question
+    fun getQuestion(id: QuestionId): Question
 }
 
 @Service
-class QuestionServiceImpl : QuestionService {
-    override fun getQuestion(questionLongId: QuestionId): Question {
-        return Question(
-            questionLongId,
-            "매쉬업에서 술 제일 잘 마실 것 같슨 사람은?",
-            QuestionType.FRIEND,
-            "image_url",
-            LocalDateTime.now(),
-            LocalDateTime.now()
-        )
+class DefaultQuestionService : QuestionService {
+    override fun getQuestion(id: QuestionId): Question {
+        // todo : questionid 를 통해 Question 불러오기 
+        return getMockQuestionSet(id)
+    }
+
+    companion object {
+        fun getMockQuestionSet(id: QuestionId): Question {
+            return Question(
+                id,
+                "매쉬업에서 술 제일 잘 마실 것 같슨 사람은?",
+                QuestionType.FRIEND,
+                "image_url",
+                LocalDateTime.now(),
+                LocalDateTime.now()
+            )
+        }
     }
 }
