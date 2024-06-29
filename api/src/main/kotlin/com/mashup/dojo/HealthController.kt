cfe83824c60fbefe82c34ba5c07e7b5e3170c96e
sw-develop <psw00719@naver.com>
2024-06-30 02:40:02 +0900

    add: healthcheck

---
+++ api/src/main/kotlin/com/mashup/dojo/HealthController.kt
@@ -0,0 +1,14 @@
+package com.mashup.dojo
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class HealthController {
+    
+    @GetMapping("/health")
+    fun health(): String {
+        return "OK"
+    }
+    
+}
