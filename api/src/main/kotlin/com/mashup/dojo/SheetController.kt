d6c005c8a83fd8fec34cf2908b4988b815b907a5
toychip <dlawnsgud427@naver.com>
2024-06-29 23:20:29 +0900

    refactor: ktlintFormat

--- api/src/main/kotlin/com/mashup/dojo/SheetController.kt
+++ api/src/main/kotlin/com/mashup/dojo/SheetController.kt
@@ -1,8 +1,8 @@
 package com.mashup.dojo
 
 import com.mashup.dojo.common.DojoApiResponse
-import com.mashup.dojo.dto.SheetSingleResponse
 import com.mashup.dojo.dto.SheetResponse
+import com.mashup.dojo.dto.SheetSingleResponse
 import com.mashup.dojo.usecase.SheetUseCase
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
@@ -11,23 +11,23 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @RequestMapping("/api/sheet")
 class SheetController(
-    private val sheetUseCase: SheetUseCase
+    private val sheetUseCase: SheetUseCase,
 ) {
-
     // 질문지 목록 12개를 반환하는 API
     @GetMapping
     fun getQuestionSheet(): DojoApiResponse<SheetResponse> {
         val generateSheets = sheetUseCase.generateSheet()
-        val response = generateSheets.map { questionSheet ->
-            SheetSingleResponse(
-                questionSheetId = questionSheet.questionSheetId,
-                currentQuestionIndex = questionSheet.currentQuestionIndex,
-                questionId = questionSheet.questionId,
-                questionContent = questionSheet.questionContent,
-                imageUrl = questionSheet.imageUrl,
-                candidates = questionSheet.candidates
-            )
-        }
+        val response =
+            generateSheets.map { questionSheet ->
+                SheetSingleResponse(
+                    questionSheetId = questionSheet.questionSheetId,
+                    currentQuestionIndex = questionSheet.currentQuestionIndex,
+                    questionId = questionSheet.questionId,
+                    questionContent = questionSheet.questionContent,
+                    imageUrl = questionSheet.imageUrl,
+                    candidates = questionSheet.candidates
+                )
+            }
 
         return DojoApiResponse.success(
             SheetResponse(response)
--- api/src/main/kotlin/com/mashup/dojo/dto/SheetSingleResponse.kt
+++ api/src/main/kotlin/com/mashup/dojo/dto/SheetSingleResponse.kt
@@ -3,7 +3,7 @@ package com.mashup.dojo.dto
 import com.mashup.dojo.domain.Candidate
 
 data class SheetResponse(
-    val questionSheetResponses: List<SheetSingleResponse>
+    val questionSheetResponses: List<SheetSingleResponse>,
 )
 
 data class SheetSingleResponse(
@@ -12,5 +12,5 @@ data class SheetSingleResponse(
     val questionId: Long,
     val questionContent: String,
     val imageUrl: String,
-    val candidates: List<Candidate>
+    val candidates: List<Candidate>,
 )
