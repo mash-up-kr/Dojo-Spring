7dac268db5da27f7889db56b47ae485b75d2325a
sw-develop <psw00719@naver.com>
2024-07-01 23:03:11 +0900

    mod: healthcheck

--- api/src/main/kotlin/com/mashup/dojo/HealthController.kt
+++ api/src/main/kotlin/com/mashup/dojo/HealthController.kt
@@ -5,10 +5,8 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class HealthController {
-    
     @GetMapping("/health")
     fun health(): String {
         return "OK"
     }
-    
 }
