b6da060855096227bc612710aaa585dea8e65499
xonmin <han51361@gmail.com>
2024-07-04 01:10:11 +0900

    add : get received pick list http

--- api/src/main/kotlin/com/mashup/dojo/PickController.kt
+++ api/src/main/kotlin/com/mashup/dojo/PickController.kt
@@ -10,12 +10,14 @@ import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.responses.ApiResponse
 import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 
 @Tag(name = "Pick", description = "픽!")
 @RestController
+@RequestMapping("/pick")
 class PickController(
     private val pickUseCase: PickUseCase,
 ) {
