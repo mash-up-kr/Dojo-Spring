e76d28dc2775e0fa8efe0051c516c16e8a23c75d
toychip <dlawnsgud427@naver.com>
2024-07-06 04:24:19 +0900

    feat: add findByValue method to Platform and Gender enums

--- entity/src/main/kotlin/com/mashup/dojo/member/Gender.kt
+++ entity/src/main/kotlin/com/mashup/dojo/member/Gender.kt
@@ -1,6 +1,18 @@
 package com.mashup.dojo.member
 
+import java.lang.RuntimeException
+
 enum class Gender {
     MALE,
     FEMALE,
+    ;
+
+    companion object {
+        fun findByValue(value: String): Gender {
+            return Gender.entries.find { it.name.equals(value, ignoreCase = true) }
+                ?: throw RuntimeException("ToDo change to DoJoException")
+            // ToDo
+            // DojoException.of(DojoExceptionType.INVALID_MEMBER_PLATFORM)
+        }
+    }
 }
