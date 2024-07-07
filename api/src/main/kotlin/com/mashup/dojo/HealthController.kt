ec6f5eb13add6f09350896eb31859c0de1e42c87
sw-develop <psw00719@naver.com>
2024-07-07 22:06:44 +0900

    add: 헬스체크 API Swagger 노출 제거

--- api/src/main/kotlin/com/mashup/dojo/HealthController.kt
+++ api/src/main/kotlin/com/mashup/dojo/HealthController.kt
@@ -1,10 +1,12 @@
 package com.mashup.dojo
 
+import io.swagger.v3.oas.annotations.Hidden
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class HealthController {
+    @Hidden
     @GetMapping("/health")
     fun health(): String {
         return "OK"
