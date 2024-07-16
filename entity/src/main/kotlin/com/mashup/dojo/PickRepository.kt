84b2b0aad2ddcc0e66f0989e11f75b5d0ed5bb67
xonmin <han51361@gmail.com>
2024-07-17 00:41:48 +0900

    add : pick entity & repository

--- entity/src/main/kotlin/com/mashup/dojo/PickRepository.kt
+++ entity/src/main/kotlin/com/mashup/dojo/PickRepository.kt
@@ -1,4 +1,21 @@
 package com.mashup.dojo
 
-interface PickRepository {
-}
+import com.mashup.dojo.base.BaseTimeEntity
+import jakarta.persistence.Entity
+import jakarta.persistence.Id
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface PickRepository : JpaRepository<PickEntity, String>
+
+@Entity
+class PickEntity(
+    @Id
+    val id: String,
+    val questionId: String,
+    val pickerId: String,
+    val pickedId: String,
+    val isGenderOpen: Boolean,
+    val isPlatformOpen: Boolean,
+    val isMidInitialNameOpen: Boolean,
+    val isFullNameOpen: Boolean,
+) : BaseTimeEntity()
