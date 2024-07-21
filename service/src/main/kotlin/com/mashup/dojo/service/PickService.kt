062b5f5dfb169da626e4119613e360c057f5de72
sw-develop <psw00719@naver.com>
2024-07-21 14:56:13 +0900

    mod: ktlint formatting

--- service/src/main/kotlin/com/mashup/dojo/service/PickService.kt
+++ service/src/main/kotlin/com/mashup/dojo/service/PickService.kt
@@ -19,13 +19,13 @@ interface PickService {
     fun openPick(
         pickId: PickId,
         pickedId: MemberId,
-        pickOpenItem: PickOpenItem
+        pickOpenItem: PickOpenItem,
     ): String
 }
 
 @Transactional(readOnly = true)
 @Service
-class DefaultPickService: PickService {
+class DefaultPickService : PickService {
     override fun getReceivedPickList(
         pickedMemberId: MemberId,
         sort: PickSort,
@@ -39,7 +39,7 @@ class DefaultPickService: PickService {
     override fun openPick(
         pickId: PickId,
         pickedId: MemberId,
-        pickOpenItem: PickOpenItem
+        pickOpenItem: PickOpenItem,
     ): String {
         // todo : 로직 구현
         // Pick 조회
@@ -47,9 +47,8 @@ class DefaultPickService: PickService {
         // Pick 유효성 검사
         // - 전달된 pickedId와 조회된 Pick.pickedId 값이 같지 않을 때 : 예외 반환
         // Pick Open 처리
-        // - 오픈하지 않은 정보일 때 : 해당 정보 오픈 처리, 해당 정보 반환  
-        // - 이미 오픈한 정보일 때 : 예외 반환 
-        
+        // - 오픈하지 않은 정보일 때 : 해당 정보 오픈 처리, 해당 정보 반환
+        // - 이미 오픈한 정보일 때 : 예외 반환
         return "MOCK_PICK_INFO_VALUE"
     }
 
