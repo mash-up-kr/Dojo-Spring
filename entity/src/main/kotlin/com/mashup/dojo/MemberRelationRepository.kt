506fb8017136c86513afa468f75fcb000b6b2bb7
toychip <dlawnsgud427@naver.com>
2024-07-28 18:59:41 +0900

    feat: fromId, toId 사용해 MemberRelation 반환, fromId 사용해 모든 MemberRelation 조회

--- entity/src/main/kotlin/com/mashup/dojo/MemberRelationRepository.kt
+++ entity/src/main/kotlin/com/mashup/dojo/MemberRelationRepository.kt
@@ -11,10 +11,35 @@ import jakarta.persistence.Table
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface MemberRelationRepository : JpaRepository<MemberRelationEntity, String>, MemberRelationQueryRepository {
-    fun findByFromId(fromId: String): List<MemberRelationEntity>
+    fun findByFromIdAndToId(
+        fromId: String,
+        toId: String,
+    ): MemberRelationEntity?
+}
+
+@Entity
+@Table(name = "member_relation")
+class MemberRelationEntity(
+    @Id
+    @Column(name = "id", nullable = false)
+    val id: String,
+    @Column(name = "from_id", nullable = false)
+    val fromId: String,
+    @Column(name = "to_id", nullable = false)
+    val toId: String,
+    @Enumerated(EnumType.STRING)
+    @Column(name = "relation_type", nullable = false)
+    val relationType: RelationType,
+) : BaseTimeEntity()
+
+enum class RelationType {
+    FRIEND,
+    ACCOMPANY,
 }
 
 interface MemberRelationQueryRepository {
+    fun findByFromId(fromId: String): List<String>
+
     fun findFriendsByFromId(fromId: String): List<String>
 
     fun findAccompanyByFromId(fromId: String): List<String>
@@ -23,6 +48,18 @@ interface MemberRelationQueryRepository {
 class MemberRelationQueryRepositoryImpl(
     private val jpaQueryFactory: JPAQueryFactory,
 ) : MemberRelationQueryRepository {
+    override fun findByFromId(fromId: String): List<String> {
+        val memberRelation = QMemberRelationEntity.memberRelationEntity
+
+        return jpaQueryFactory
+            .select(memberRelation.toId)
+            .from(memberRelation)
+            .where(
+                memberRelation.fromId.eq(fromId)
+            )
+            .fetch()
+    }
+
     override fun findFriendsByFromId(fromId: String): List<String> {
         return findByFromIdAndRelationType(fromId, RelationType.FRIEND)
     }
@@ -46,24 +83,4 @@ class MemberRelationQueryRepositoryImpl(
             )
             .fetch()
     }
-}
-
-@Entity
-@Table(name = "member_relation")
-class MemberRelationEntity(
-    @Id
-    @Column(name = "id", nullable = false)
-    val id: String,
-    @Column(name = "from_id", nullable = false)
-    val fromId: String,
-    @Column(name = "to_id", nullable = false)
-    val toId: String,
-    @Enumerated(EnumType.STRING)
-    @Column(name = "relation_type", nullable = false)
-    val relationType: RelationType,
-) : BaseTimeEntity()
-
-enum class RelationType {
-    FRIEND,
-    ACCOMPANY,
 }
