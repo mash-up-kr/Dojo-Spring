14313e2a5db6347cd76fcd3108f0e84465d4576c
xonmin <han51361@gmail.com>
2024-07-30 11:15:25 +0900

    add : questionSheet 조회 응답 모델

--- api/src/main/kotlin/com/mashup/dojo/dto/QuestionDto.kt
+++ api/src/main/kotlin/com/mashup/dojo/dto/QuestionDto.kt
@@ -1,8 +1,11 @@
 package com.mashup.dojo.dto
 
 import com.mashup.dojo.domain.ImageId
+import com.mashup.dojo.domain.MemberId
 import com.mashup.dojo.domain.QuestionCategory
 import com.mashup.dojo.domain.QuestionId
+import com.mashup.dojo.domain.QuestionSetId
+import com.mashup.dojo.domain.QuestionSheetId
 import com.mashup.dojo.domain.QuestionType
 import io.swagger.v3.oas.annotations.media.Schema
 import jakarta.validation.constraints.NotBlank
@@ -36,4 +39,72 @@ data class QuestionSetCustomCreateRequest(
     val questionIds: List<QuestionId>,
     @Schema(description = "질문지 세트를 발행할 시각")
     val publishedAt: LocalDateTime,
+)
+
+
+@Schema(description = "질문지 (투표 용지) 한 다스 조회 응답")
+data class QuestionSheetsGetResponse(
+    @Schema(description = "응답자 id")
+    val resolverId: MemberId,
+    @Schema(description = "질문지 한 묶음의 고유 식별 값")
+    val questionSetId: QuestionSetId,
+    @Schema(description = "한 묶음에 해당하는 전체 질문지 수. 1based")
+    val sheetTotalCount: Int,
+    @Schema(description = "유저가 풀어야하는 질문지 순서(1based). 기존에 푼 적이 없다면 1부터 ~ sheetTotalCount 까지")
+    val startingQuestionIndex: Int,
+    @Schema(description = "질문지 상세 리스트")
+    val questionList: List<QuestionSheetResponse>
+)
+
+@Schema(description = "질문지 상세")
+data class QuestionSheetResponse(
+    @Schema(description = "질문지에 대한 고유 식별 값")
+    val questionSheetId: QuestionSheetId,
+    @Schema(description = "질문지에 해당하는 응답자 id")
+    val resolverId: MemberId,
+    @Schema(description = "질문 고유 id")
+    val questionId: QuestionId,
+    @Schema(description = "질문지 묶음 중 해당 질문지의 순서")
+    val questionOrder: Int,
+    @Schema(description = "질문 내용")
+    val questionContent: String,
+    @Schema(description = "질문 카테고리")
+    val questionCategory: String,
+    @Schema(description = "질문 카테고리에 맞는 이모지 이미지")
+    val questionEmojiImage : ByteArray,
+    @Schema(description = "질문에 대한 후보자 list")
+    val candidates: List<QuestionSheetCandidate>
+) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as QuestionSheetResponse
+
+        if (questionSheetId != other.questionSheetId) return false
+        if (resolverId != other.resolverId) return false
+        if (questionId != other.questionId) return false
+        if (questionOrder != other.questionOrder) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = questionSheetId.hashCode()
+        result = 31 * result + resolverId.hashCode()
+        result = 31 * result + questionId.hashCode()
+        result = 31 * result + questionOrder
+        return result
+    }
+}
+@Schema(description = "질문에 대한 후보자")
+data class QuestionSheetCandidate(
+    @Schema(description = "후보자 id")
+    val memberId: MemberId,
+    @Schema(description = "후보자 리스트 중 해당 후보자의 순서")
+    val order: Int,
+    @Schema(description = "후보자 이름")
+    val memberName: String,
+    @Schema(description = "후보자 플랫폼")
+    val platform: String,
 )
--- service/src/main/kotlin/com/mashup/dojo/usecase/QuestionUseCase.kt
+++ service/src/main/kotlin/com/mashup/dojo/usecase/QuestionUseCase.kt
@@ -31,6 +31,12 @@ interface QuestionUseCase {
         val publishedAt: LocalDateTime,
     )
 
+    data class GetQuestionSheetsResult(
+        val resolverId: MemberId,
+        // 문제지 전체 
+        val sheetTotalIndex: Int, 
+        val startResolveIndex: Int, 
+    )
     fun create(command: CreateCommand): Question
 
     fun bulkCreate(commands: List<CreateCommand>): List<Question>
@@ -42,6 +48,7 @@ interface QuestionUseCase {
     fun createQuestionSheet(): List<QuestionSheet>
 
     fun getQuestionSheetList(memberId: MemberId): List<QuestionSheet>
+    
 }
 
 @Component
@@ -85,24 +92,26 @@ class DefaultQuestionUseCase(
 
     override fun getQuestionSheetList(memberId: MemberId): List<QuestionSheet> {
         // 운영중인 questionSet 조회 (todo : scheduler 가 최신 QuestionSet 을 발행 시각 2분전에 publishedYn Y 로 변경 예정)
-        val operatingQSet = questionService.getOperatingQuestionSet() ?: throw DojoException.of(DojoExceptionType.QUESTION_SET_OPERATING_NOT_EXIST)
+        val operatingQSet = questionService.getOperatingQuestionSet()
+            ?: throw DojoException.of(DojoExceptionType.QUESTION_SET_OPERATING_NOT_EXIST)
 
         // QuestionSetId & solverId 를 통해 현재 운영중인 QuestionSet 에서 푼 문제가 어디까지 인지 확인 
         // todo : Pick 정보에 QuestionSetId 필요함 (QuestionSet 이 없는 경우, 동일 질문 다른 후보자에 대한 정보와 분간 X) 
-        val solvedQuestionId = pickService.getSolvedPickList(memberId, operatingQSet.id).map { it.questionId }
+        val solvedQuestionIds = pickService.getSolvedPickList(memberId, operatingQSet.id)
+            .map { it.questionId }
 
         // QuestionSheet 조회 후 푼 문제지 필터링 
         return questionService.getQuestionSheets(memberId, operatingQSet.id)
-            .filter { it.questionId !in solvedQuestionId }
+            .filterNot { it.questionId in solvedQuestionIds }
             .map { qSheet ->
                 val candidates = qSheet.candidates.mapIndexed { index, memberId ->
-                    val member = memberService.findMemberById(memberId) ?: throw DojoException.of(DojoExceptionType.MEMBER_NOT_FOUND)
-
-                    Candidate(
-                        memberId = member.id,
-                        memberName = member.fullName,
-                        order = index
-                    )
+                    memberService.findMemberById(memberId)?.let { member ->
+                        Candidate(
+                            memberId = member.id,
+                            memberName = member.fullName,
+                            order = index
+                        )
+                    } ?: throw DojoException.of(DojoExceptionType.MEMBER_NOT_FOUND)
                 }
 
                 qSheet.toQuestionSheet(candidates)
