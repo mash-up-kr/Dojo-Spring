1cd73ef2910607737fc054d4e0fb9f5b612a65c2
Heidi <heidi@daangn.com>
2024-08-02 23:51:29 +0900

    lint

--- api/src/main/kotlin/com/mashup/dojo/config/security/MemberAuthTokenAuthenticationFilter.kt
+++ api/src/main/kotlin/com/mashup/dojo/config/security/MemberAuthTokenAuthenticationFilter.kt
@@ -2,7 +2,6 @@ package com.mashup.dojo.config.security
 
 import com.mashup.dojo.DojoException
 import com.mashup.dojo.DojoExceptionType
-import com.mashup.dojo.domain.MemberId
 import jakarta.servlet.FilterChain
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
@@ -11,13 +10,18 @@ import org.springframework.web.filter.OncePerRequestFilter
 
 class MemberAuthTokenAuthenticationFilter(
     private val memberAuthTokenAuthenticationProvider: MemberAuthTokenAuthenticationProvider,
-): OncePerRequestFilter() {
-    override fun doFilterInternal(request: HttpServletRequest, response: HttpServletResponse, filterChain: FilterChain) {
+) : OncePerRequestFilter() {
+    override fun doFilterInternal(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        filterChain: FilterChain,
+    ) {
         // Todo Path 분리 밖에서 할 수 있도록 변경하기
         // path가 public 으로 시작하면 패스
         val isPublicPath = request.servletPath.startsWith("/public")
-        if(isPublicPath) filterChain.doFilter(request, response)
-        else {
+        if (isPublicPath) {
+            filterChain.doFilter(request, response)
+        } else {
             val token = resolveMemberAuthToken(request) ?: throw DojoException.of(DojoExceptionType.AUTHENTICATION_FAILURE, "토큰을 찾을 수 없어요")
             val memberPrincipal = memberAuthTokenAuthenticationProvider.authenticate(token)
 
@@ -26,15 +30,13 @@ class MemberAuthTokenAuthenticationFilter(
     }
 
     private fun resolveMemberAuthToken(request: HttpServletRequest): MemberAuthToken? {
-        return kotlin.runCatching {  
+        return kotlin.runCatching {
             val token = request.getHeader(AUTHORIZATION_HEADER_NAME)
             MemberAuthToken(token)
         }.getOrNull()
     }
-    
+
     companion object {
-        private const val AUTHORIZATION_HEADER_NAME = "Authorization";
+        private const val AUTHORIZATION_HEADER_NAME = "Authorization"
     }
 }
-
-
