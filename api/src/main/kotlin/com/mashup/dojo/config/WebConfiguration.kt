3555a20b15c65c08bd69331ab90a566e66938403
Heidi <heidi@daangn.com>
2024-08-02 23:16:08 +0900

    feat; localhost:5173 CORS 허용해요

---
+++ api/src/main/kotlin/com/mashup/dojo/config/WebConfiguration.kt
@@ -0,0 +1,14 @@
+package com.mashup.dojo.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class WebConfiguration : WebMvcConfigurer {
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+            .allowedOrigins("http://localhost:5173")
+            .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE")
+    }
+}
