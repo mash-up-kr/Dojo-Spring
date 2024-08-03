57ab543fc937c10b342b2f0da85c9cbf0da29fa9
toychip <dlawnsgud427@naver.com>
2024-08-04 00:54:46 +0900

    feat: PickDetail Page Query

--- entity/src/main/kotlin/com/mashup/dojo/PickRepository.kt
+++ entity/src/main/kotlin/com/mashup/dojo/PickRepository.kt
@@ -1,10 +1,15 @@
 package com.mashup.dojo
 
 import com.mashup.dojo.base.BaseTimeEntity
+import com.querydsl.core.types.dsl.Wildcard
+import com.querydsl.jpa.impl.JPAQueryFactory
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
 import jakarta.persistence.Id
 import jakarta.persistence.Table
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.PageImpl
+import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface PickRepository : JpaRepository<PickEntity, String> {
@@ -35,3 +40,65 @@ class PickEntity(
     @Column(name = "is_full_name_open")
     val isFullNameOpen: Boolean = false,
 ) : BaseTimeEntity()
+
+interface PickRepositoryCustom {
+    fun findPickDetailPaging(
+        memberId: String,
+        questionId: String,
+        pageable: Pageable,
+    ): Page<PickEntity>
+
+    fun findPickDetailContent(
+        memberId: String,
+        questionId: String,
+        pageable: Pageable,
+    ): List<PickEntity>
+
+    fun findPickDetailCount(
+        memberId: String,
+        questionId: String,
+    ): Long
+}
+
+class PickRepositoryImpl(
+    private val jpaQueryFactory: JPAQueryFactory,
+) : PickRepositoryCustom {
+    override fun findPickDetailPaging(
+        memberId: String,
+        questionId: String,
+        pageable: Pageable,
+    ): Page<PickEntity> {
+        val picks = findPickDetailContent(memberId = memberId, questionId = questionId, pageable = pageable)
+        val count = findPickDetailCount(memberId = memberId, questionId = questionId)
+
+        return PageImpl(picks, pageable, count)
+    }
+
+    override fun findPickDetailContent(
+        memberId: String,
+        questionId: String,
+        pageable: Pageable,
+    ): List<PickEntity> {
+        val pickEntity = QPickEntity.pickEntity
+        return jpaQueryFactory
+            .selectFrom(pickEntity)
+            .where(
+                pickEntity.pickedId.eq(memberId),
+                pickEntity.questionId.eq(questionId)
+            )
+            .offset(pageable.offset)
+            .limit(pageable.pageSize.toLong())
+            .fetch()
+    }
+
+    override fun findPickDetailCount(
+        memberId: String,
+        questionId: String,
+    ): Long {
+        val pickEntity = QPickEntity.pickEntity
+        return jpaQueryFactory
+            .select(Wildcard.count)
+            .from(pickEntity)
+            .fetchOne() ?: 0
+    }
+}
