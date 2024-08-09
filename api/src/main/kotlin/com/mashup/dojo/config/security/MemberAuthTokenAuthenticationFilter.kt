bf0b6cc3df50afa5a07ac82e2a2066efa7023676
Heidi <heidi@daangn.com>
2024-08-09 22:53:19 +0900

    필터 제외 path에 health 추가하기

--- api/src/main/kotlin/com/mashup/dojo/config/security/MemberAuthTokenAuthenticationFilter.kt
+++ api/src/main/kotlin/com/mashup/dojo/config/security/MemberAuthTokenAuthenticationFilter.kt
@@ -21,8 +21,12 @@ class MemberAuthTokenAuthenticationFilter(
     ) {
         try {
             // Todo Path 분리 밖에서 할 수 있도록 변경하기
-            // path가 public 으로 시작하면 패스
-            val isPublicPath = request.servletPath.startsWith("/public") || request.servletPath.startsWith("/swagger") || request.servletPath.startsWith("/v3/api-docs")
+            val isPublicPath =
+                request.servletPath.startsWith("/public") ||
+                    request.servletPath.startsWith("/swagger") ||
+                    request.servletPath.startsWith("/v3/api-docs") ||
+                    request.servletPath.startsWith("/health")
+
             if (isPublicPath) {
                 filterChain.doFilter(request, response)
             } else {
