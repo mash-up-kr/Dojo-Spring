adfc4833c66d2feafc34f3e817e2cde4c11a9c8f
Heidi <heidi@daangn.com>
2024-08-09 22:49:53 +0900

    fix; health-check endpoint에 public을 붙여요

--- api/src/main/kotlin/com/mashup/dojo/HealthController.kt
+++ api/src/main/kotlin/com/mashup/dojo/HealthController.kt
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 class HealthController {
     @Hidden
-    @GetMapping("/health")
+    @GetMapping("/public/health")
     fun health(): String {
         return "OK"
     }
