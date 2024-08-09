c0b4d8a453441c09fdcd5c01cdd15cb8c8d8913d
toychip <dlawnsgud427@naver.com>
2024-08-09 16:15:47 +0900

    fix: profile api memberId RequestParam to PathVariable

--- api/src/main/kotlin/com/mashup/dojo/MemberController.kt
+++ api/src/main/kotlin/com/mashup/dojo/MemberController.kt
@@ -18,7 +18,6 @@ import org.springframework.web.bind.annotation.PatchMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 private val logger = KotlinLogging.logger { }
@@ -76,13 +75,13 @@ class MemberController(
         return DojoApiResponse.success(MemberLoginResponse(id, authToken.credentials))
     }
 
-    @GetMapping("/member")
+    @GetMapping("/member/{memberId}")
     @Operation(
         summary = "타인 멤버 프로필 조회 API",
         description = "멤버의 프로필을 조회하는 API."
     )
     fun getProfile(
-        @RequestParam memberId: String,
+        @PathVariable memberId: String,
     ): DojoApiResponse<MemberProfileResponse> {
         val profileResponse = memberUseCase.findMemberById(MemberId(memberId))
 
