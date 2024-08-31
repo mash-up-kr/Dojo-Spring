d7d6d9c99eeaf3ec3adef520dbc54a4a7f3a015e
toychip <dlawnsgud427@naver.com>
2024-08-31 14:39:33 +0900

    feat: TimeLogger

---
+++ api/src/main/kotlin/com/mashup/dojo/common/TimeLogger.kt
@@ -0,0 +1,21 @@
+package com.mashup.dojo.common
+
+import jakarta.annotation.PostConstruct
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.stereotype.Component
+import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
+
+@Component
+class TimeLogger {
+
+    private val logger: Logger = LoggerFactory.getLogger(TimeLogger::class.java)
+
+    @PostConstruct
+    fun logCurrentTime() {
+        val now = ZonedDateTime.now()
+        val formattedTime = now.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss Z"))
+        logger.info("Application started at: $formattedTime")
+    }
+}
