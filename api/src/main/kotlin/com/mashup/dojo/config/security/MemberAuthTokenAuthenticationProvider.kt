9573d55710b9b8f8e4043f327eb4580c521f895a
toychip <dlawnsgud427@naver.com>
2024-09-06 23:36:09 +0900

    fix: 투표화면 모든 회원 접근 가능하도록 변경

--- api/src/main/kotlin/com/mashup/dojo/config/security/MemberAuthTokenAuthenticationProvider.kt
+++ api/src/main/kotlin/com/mashup/dojo/config/security/MemberAuthTokenAuthenticationProvider.kt
@@ -17,18 +17,18 @@ class MemberAuthTokenAuthenticationProvider(
         val memberId = jwtTokenService.getMemberId(authToken) ?: throw DojoException.of(AUTHENTICATION_FAILURE, "invalid token")
         val member = memberService.findMemberById(memberId) ?: throw DojoException.of(MEMBER_NOT_FOUND)
 
-        notAllowMember(member.fullName)
+        // notAllowMember(member.fullName)
 
         return MemberPrincipal(member)
     }
 
-    private fun notAllowMember(fullName: String) {
-        val dojo = listOf("한정민", "오예원", "박세원", "임준형", "이현재", "황태규", "최민석", "낭은영", "오시연")
-
-        if (fullName !in dojo) {
-            throw DojoException.of(AUTHENTICATION_FAILURE, "You Are Not Dojo")
-        }
-    }
+    // private fun notAllowMember(fullName: String) {
+    //     val dojo = listOf("한정민", "오예원", "박세원", "임준형", "이현재", "황태규", "최민석", "낭은영", "오시연")
+    //
+    //     if (fullName !in dojo) {
+    //         throw DojoException.of(AUTHENTICATION_FAILURE, "You Are Not Dojo")
+    //     }
+    // }
 }
 
 class MemberPrincipal(
