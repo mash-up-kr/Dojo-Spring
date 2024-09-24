38ce9ae8c96827e0470ec98d945cbbe175a2e9ef
toychip <dlawnsgud427@naver.com>
2024-09-24 18:28:27 +0900

    fix: 친구 삭제 fromMemberId token에서 꺼내도록 수정

--- api/src/main/kotlin/com/mashup/dojo/dto/MemberCreateRequest.kt
+++ api/src/main/kotlin/com/mashup/dojo/dto/MemberCreateRequest.kt
@@ -39,9 +39,6 @@ data class MemberCreateFriendRelationRequest(
 @Schema(description = "팔로우 해제 요청")
 data class MemberDeleteFriendRelationRequest(
     @field:NotBlank
-    @Schema(description = "팔로우 해제 요청한 유저 id")
-    val fromMemberId: MemberId,
-    @field:NotBlank
     @Schema(description = "팔로우 해제 대상 유저 id")
     val toMemberId: MemberId,
 )
